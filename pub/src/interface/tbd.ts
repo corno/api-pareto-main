ed85706d21292529eda8e7452a1e80a0001e8a96
corno <corno.astn.a@schraverus.com>
2022-09-01 12:10:09 +0200

    first commit

---
+++ pub/src/interface/tbd.ts
@@ -0,0 +1,18 @@
+import * as pt from "pareto-core-types"
+
+export type ProgramMain = (
+    $: {
+        arguments: pt.Array<string>
+    },
+    $i: {
+        stdout: {
+            write: ($: string) => void
+        },
+        stderr: {
+            write: ($: string) => void
+        }
+    },
+    $d: {
+        startAsync: ($: pt.AsyncNonValue) => void
+    }
+) => void
