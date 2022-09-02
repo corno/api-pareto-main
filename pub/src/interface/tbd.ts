21b2fd758bb62e8ae0fe01d0110e159530b64a53
corno <corno.astn.a@schraverus.com>
2022-09-02 11:09:44 +0200

    .

--- pub/src/interface/tbd.ts
+++ pub/src/interface/tbd.ts
@@ -1,21 +1,24 @@
 import * as pt from "pareto-core-types"
 
+export type Arguments = pt.Array<string>
+
+export type WriteStream = {
+    write: ($: string) => void
+}
+export type StartAsync = ($: pt.AsyncNonValue) => void
+export type SetExitCodeToFailed = () => void
+
 export type ProgramMain = (
     $: {
-        arguments: pt.Array<string>
+        arguments: Arguments
     },
     $i: {
-        stdout: {
-            write: ($: string) => void
-        },
-        stderr: {
-            write: ($: string) => void
-        }
+        stdout: WriteStream
+        stderr: WriteStream
     },
     $d: {
         startAsync: StartAsync
-        setExitCodeToFailed: () => void
+        setExitCodeToFailed: SetExitCodeToFailed
     }
 ) => void
 
-export type StartAsync = ($: pt.AsyncNonValue) => void
